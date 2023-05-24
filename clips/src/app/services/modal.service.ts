ce9968c029bf07882c06aa578327b04a7d213bba
GuptaSatyanshuThinksys <gupta.satyanshu@thinksys.com>
2023-05-24 12:38:47 +0530

    opening two different modal seperately

--- clips/src/app/services/modal.service.ts
+++ clips/src/app/services/modal.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
 
+interface Imodal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
-  visible = false;
+  private modals: Imodal[] = [];
 
   constructor() { }
+
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false
+    })
+  }
+
+  isModalOpen(id: string): boolean {
+    // Boolean(this.modals.find(element => element.id === id)?.visible)
+    return !!this.modals.find(element => element.id === id)?.visible
+  }
+
+
+  toggleModal(id: string) {
+    const modal = this.modals.find(element => element.id === id)
+
+    if (modal) {
+      modal.visible = !modal.visible
+    }
+  }
 }
