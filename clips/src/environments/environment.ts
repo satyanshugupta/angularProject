6a92a3f14bdd2ab158943e3265ef6abd1eb301a8
GuptaSatyanshuThinksys <gupta.satyanshu@thinksys.com>
2023-06-06 10:10:31 +0530

    installed Angular firebase

---
+++ clips/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyDTONXLSTgecM2pUgdGe5yRBGC7PTBpu0w",
+        authDomain: "clips-78a98.firebaseapp.com",
+        projectId: "clips-78a98",
+        storageBucket: "clips-78a98.appspot.com",
+        // messagingSenderId: "333327004506",
+        appId: "1:333327004506:web:5d3c488c27b057e1593963"
+      }
+};
